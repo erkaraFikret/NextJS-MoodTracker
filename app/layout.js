8cc7d1c7ff4c90ece88f4c0ed428f53a7f15e595
Fikret Erkara <fikreterkara_26@hotmail.com>
2024-10-02 16:39:56 +0300

    created header, hero and footer components

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,41 @@
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
+import { Fugaz_One, Open_Sans } from "next/font/google";
+
+
+
+const openSans = Open_Sans({ subsets: ["latin"]});
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
-  title: "NextMoods",
+  title: "Moody",
   description: "Track your daily mood every day of the year!",
 };
 
 export default function RootLayout({ children }) {
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1
+        className={`text-base sm:text-lg font-bold textGradient transform transition-transform hover:scale-105  ${fugaz.className}`}
+      >
+        Moody
+      </h1>
+    </header>
+  );
+
+  const footer = <footer className="p-4 sm:p-8">
+    <p className={'text-center text-indigo-500 ' + fugaz.className}>Build by Fikret Erkara 💛</p>
+  </footer>;
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`
+          ${openSans.className} 
+          antialiased w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800`}
       >
-        <header>Header</header>
+        {header}
         {children}
-        <footer>Footer</footer>
+        {footer}
       </body>
     </html>
   );
--- components/Main.jsx
+++ components/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Main = (props) => {
     const { children } = props;
     return (
-        <main>
+        <main className='flex-1 flex flex-col p-4 sm:p-8'>
             {children}
         </main>
     )
